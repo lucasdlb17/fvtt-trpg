b5d2859c3160024e658181677987a9a7df2ca85f
lucasdlb17 <lucasdiaslimabraga@hotmail.com>
2022-07-01 12:35:25 -0300

    Update config.js

--- module/config.js
+++ module/config.js
@@ -266,10 +266,10 @@ TRPG.armorTypes = {
  * @type {Object}
  */
 TRPG.equipmentTypes = {
-    bonus: "TRPG.EquipmentBonus",
-    clothing: "TRPG.EquipmentClothing",
-    trinket: "TRPG.EquipmentTrinket",
-    vehicle: "TRPG.EquipmentVehicle",
+	bonus: "TRPG.EquipmentBonus",
+	clothing: "TRPG.EquipmentClothing",
+	trinket: "TRPG.EquipmentTrinket",
+	vehicle: "TRPG.EquipmentVehicle",
 };
 
 /* -------------------------------------------- */
@@ -291,10 +291,10 @@ TRPG.vehicleTypes = {
  * @type {Object}
  */
 TRPG.armorProficiencies = {
-    lgt: "TRPG.EquipmentLightProficiency",
-    med: "TRPG.EquipmentMediumProficiency",
-    hvy: "TRPG.EquipmentHeavyProficiency",
-    shl: "TRPG.EquipmentShieldProficiency",
+	lgt: "TRPG.EquipmentLightProficiency",
+	med: "TRPG.EquipmentMediumProficiency",
+	hvy: "TRPG.EquipmentHeavyProficiency",
+	shl: "TRPG.EquipmentShieldProficiency",
 };
 
 /**
@@ -383,7 +383,7 @@ TRPG.consumableTypes = {
 /* -------------------------------------------- */
 
 /**
- * The valid currency denominations supported by the system
+ * The valid currency denominations supported by the 5e system
  * @type {Object}
  */
 TRPG.currencies = {
@@ -395,13 +395,13 @@ TRPG.currencies = {
 };
 
 /**
- * The valid currency denominations supported by the system
+ * The valid currency denominations supported by the 5e system
  * @type {Object}
  */
-TRPG.currenciesIdJ = {
-	gp: "TRPG.Currency¥o",
-	sp: "TRPG.Currency¥p",
-	cp: "TRPG.Currency¥",
+TRPG.currenciesIDJ = {
+	gp: "TRPG.CurrencyYo",
+	sp: "TRPG.CurrencyYp",
+	cp: "TRPG.CurrencyY",
 };
 
 /**
@@ -703,15 +703,8 @@ TRPG.spellLists = {
 };
 // Spell Schools
 TRPG.spellSchools = {
-	abj: "TRPG.SchoolAbj",
-	con: "TRPG.SchoolCon",
-	div: "TRPG.SchoolDiv",
-	enc: "TRPG.SchoolEnc",
-	// "evo": "TRPG.SchoolEvo",
-	ill: "TRPG.SchoolIll",
-	nec: "TRPG.SchoolNec",
-	trs: "TRPG.SchoolTrs",
-	acid: "TRPG.SchoolAcid",
+	abj: "TRPG.SchoolAbjuracao",
+	acid: "TRPG.SchoolAcido",
 	agua: "TRPG.SchoolAgua",
 	ar: "TRPG.SchoolAr",
 	elet: "TRPG.SchoolEletricidade",
@@ -720,15 +713,39 @@ TRPG.spellSchools = {
 	frio: "TRPG.SchoolFrio",
 	sonic: "TRPG.SchoolSonico",
 	terra: "TRPG.SchoolTerra",
+	adv: "TRPG.SchoolAdvinhacao",
 	bem: "TRPG.SchoolBem",
 	mal: "TRPG.SchoolMal",
 	ordem: "TRPG.SchoolOrdem",
 	caos: "TRPG.SchoolCaos",
 	cura: "TRPG.SchoolCura",
+	enc: "TRPG.SchoolEncantamento",
 	escuro: "TRPG.SchoolEscuridao",
 	luz: "TRPG.SchoolLuz",
+	ilu: "TRPG.SchoolIllusao",
+	inv: "TRPG.SchoolInvocacao",
 	medo: "TRPG.SchoolMedo",
+	nec: "TRPG.SchoolNecromancia",
 	tempo: "TRPG.SchoolTempo",
+	trs: "TRPG.SchoolTransmutacao",
+};
+
+// Jutsu Schools
+TRPG.jutsuSchools = {
+	abj: "TRPG.SchoolAbjuracao",
+	adv: "TRPG.SchoolAdvinhacao",
+	acid: "TRPG.SchoolAcido",
+	elet: "TRPG.SchoolEletricidade",
+	fogo: "TRPG.SchoolFogo",
+	frio: "TRPG.SchoolFrio",
+	vento: "TRPG.SchoolVento",
+	enc: "TRPG.SchoolEncantamento",
+	ilu: "TRPG.SchoolIllusao",	
+	inv: "TRPG.SchoolInvocacao",
+	luz: "TRPG.SchoolLuz",
+	trs: "TRPG.SchoolTransmutacao",
+	trevas: "TRPG.SchoolTrevas",
+	vacuo: "TRPG.SchoolVacuo",
 };
 
 // Spell Levels
@@ -745,6 +762,16 @@ TRPG.spellLevels = {
 	9: "TRPG.SpellLevel9",
 };
 
+// Jutsu Levels
+TRPG.jutsuLevels = {
+	0: "TRPG.JutsuLevel0",
+	1: "TRPG.JutsuLevel1",
+	2: "TRPG.JutsuLevel2",
+	3: "TRPG.JutsuLevel3",
+	4: "TRPG.JutsuLevel4",
+	5: "TRPG.JutsuLevel5",
+};
+
 // Spell Scroll Compendium UUIDs
 TRPG.spellScrollIds = {
 	0: "rQ6sO7HDWzqMhSI3",
@@ -757,6 +784,17 @@ TRPG.spellScrollIds = {
 	7: "mtyw4NS1s7j2EJaD",
 	8: "aOrinPg7yuDZEuWr",
 	9: "O4YbkJkLlnsgUszZ",
+};
+
+// Feat Lists
+TRPG.featLists = {
+	combat: "Combate",
+	skill: "Perícia",
+	fate: "Destino",
+	spell: "Magia",
+	jutsu: "Jutsu",
+	gift: "Poder Concedido",
+	tormenta: "Tormenta"
 };
 
 /**
